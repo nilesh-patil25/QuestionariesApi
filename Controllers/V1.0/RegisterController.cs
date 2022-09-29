ef950fa4fb7fa2f98962f64bebb2f06f203470d6
nilesh-patil25 <111033833+nilesh-patil25@users.noreply.github.com>
2022-09-29 18:00:38 +0530

    Add files via upload
    Added controller to project

---
+++ Controllers/V1.0/RegisterController.cs
@@ -0,0 +1,57 @@
+﻿using TownhomeQuestionnaire.API.JWT;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using TownhomeQuestionnaire.API.Model;
+
+namespace TownhomeQuestionnaire.API.Controllers.V1._0
+{
+    [Route("api/{version:apiVersion}")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    [AllowAnonymous]
+    public class RegisterController : ControllerBase
+    {
+
+        private readonly IServiceFactory ServiceFactory;
+        public RegisterController()
+        {
+            ServiceFactory = new ServiceFactory();
+        }
+
+
+        /// <summary>
+        /// Creating user in system
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult> Register(Register register)
+        {
+            try
+            {
+                if (register == null)
+                {
+                    throw new ArgumentNullException(nameof(register), "Body can not be empty.");
+                }
+                var result = await ServiceFactory.RegisterService.RegisterUser(register);
+                if (result)
+                {
+                    return Ok(new { message = "User register successfully!" });
+                }
+                else
+                {
+                    throw new InvalidDataException("");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = "Error occured while registering!", error = ex.Message});
+            }
+        }
+
+    }
+}
---
+++ Controllers/V1.0/UserController.cs
@@ -0,0 +1,109 @@
+﻿using TownhomeQuestionnaire.API.JWT;
+using TownhomeQuestionnaire.API.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using TownhomeQuestionnaire.API.Helper;
+using Services;
+
+namespace TownhomeQuestionnaire.API.Controllers
+{
+    [Route("api/{version:apiVersion}")]
+    [ApiVersion("1")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IServiceFactory ServiceFactory;
+        public UserController()
+        {
+            ServiceFactory = new ServiceFactory();
+        }
+        
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> GetAllUsers()
+        {
+            try
+            {
+                var userList = await ServiceFactory.UserService.GetAllUsers();
+                if(userList is not null)
+                {
+                    return Ok(new {message = "User data fetched successfully", data = userList.Users.Select(x=> new { x.Name, x.Email}) });
+                }
+                else
+                {
+                    throw new Exception("user data not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error occured while fetching users",error = ex.Message });
+            }
+        }
+
+        [HttpGet("{email}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> GetUserBymail(string email)
+        {
+            try
+            {
+                var user = await ServiceFactory.UserService.GetUserByEmail(email);
+                if(user is not null)
+                {
+                    return Ok(new {message = "User data fetched successfully", data = new { user.Name, user.Email} });
+                }
+                else
+                {
+                    throw new Exception("user data not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error occured while fetching users",error = ex.Message });
+            }
+        }
+
+
+        [HttpPut("{email}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> UpdateUser(string email)
+        {
+            try
+            {
+
+                var user = await ServiceFactory.UserService.GetUserByEmail(email);
+
+                var stream = new StreamReader(Request.Body);
+                var updateJson = await stream.ReadToEndAsync();
+                var updateJObject = JObject.Parse(updateJson);
+
+                var isUpdate = ConversionHelper.PathchObject(updateJObject, user);
+                if (isUpdate)
+                {
+                    var result = await ServiceFactory.UserService.UpdateUserByEmail(email, user);
+                    if (result)
+                    {
+                        return Ok(new { message = "User data updated successfully" });
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                else
+                {
+                    return Ok(new { message = "User data is up to date!" });
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Error occured while fetching users", error = ex.Message });
+            }
+        }
+
+    }
+}
